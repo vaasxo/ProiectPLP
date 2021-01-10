4424a0cf104b3e0c8b6e9bf8e21719975c9abafc
vaasxo <xovaasxo@gmail.com>
2021-01-10 23:18:24 +0200

    worked on most of eval, completed atoi and itoa, func and obj eval left

--- PLP_Proiect_2.v
+++ PLP_Proiect_2.v
@@ -107,7 +107,8 @@ Compute object ("car") (["make","model","color"]) ("setColor" takes ["color"]).
 Inductive Value :=
 | undef : Value
 | nat_val : nat -> Value
-| bool_val : bool -> Value.
+| bool_val : bool -> Value
+| string_val : string -> Value.
 
 Scheme Equality for Value.
 
@@ -125,6 +126,8 @@ Inductive Stmt :=
 | object_declare : string -> Object -> Stmt
 | function_declare : Function -> Stmt -> Stmt
 | assignment : string -> Exp -> Stmt 
+| atoi_assignment : string -> string -> Stmt
+| itoa_assignment : string -> Exp -> Stmt
 | sequence : Stmt -> Stmt -> Stmt
 | ifthenelse : Exp -> Stmt -> Stmt -> Stmt
 | ifthen : Exp -> Stmt -> Stmt
@@ -135,6 +138,8 @@ Notation "X ::= A" := (assignment X A) (at level 90).
 Notation "S ;; S'" := (sequence S S') (at level 93, right associativity).
 Notation "A <{ B }>" := (function_declare A B) (at level 90).
 Notation "A 'is' B" := (object_declare A B) (at level 90).
+Notation "X ::= 'atoi'( Y )" := (atoi_assignment X Y) (at level 94).
+Notation "X ::= 'itoa'( Y )" := (itoa_assignment X Y) (at level 94).
 
 Example ex1 :=
 fie "c" ;;
@@ -157,6 +162,79 @@ Compute ex2.
 
 (*SEMANTICS*)
 
+(*atoi*)
+
+Open Scope char_scope.
+
+Definition digitToNat (c : ascii) : nat :=
+  match c with
+    | "0" => 0
+    | "1" => 1
+    | "2" => 2
+    | "3" => 3
+    | "4" => 4
+    | "5" => 5
+    | "6" => 6
+    | "7" => 7
+    | "8" => 8
+    | "9" => 9
+    | _ => 0
+  end.
+
+Open Scope string_scope.
+
+
+Fixpoint readNatAux (s : string) (acc : nat) : nat :=
+  match s with
+    | "" => acc
+    | String c s' =>
+      match digitToNat c with
+        | n => readNatAux s' (10 * acc + n)
+      end
+  end.
+
+Definition readNat (s : string) : nat :=
+  readNatAux s 0.
+
+Open Scope string_scope.
+
+Definition natToDigit (n : nat) : ascii :=
+  match n with
+    | 0 => "0"
+    | 1 => "1"
+    | 2 => "2"
+    | 3 => "3"
+    | 4 => "4"
+    | 5 => "5"
+    | 6 => "6"
+    | 7 => "7"
+    | 8 => "8"
+    | _ => "9"
+  end.
+
+Fixpoint writeNatAux (time n : nat) (acc : string) : string :=
+  let acc' := String (natToDigit (Nat.modulo n 10)) acc in
+  match time with
+    | 0 => acc'
+    | S time' =>
+      match (Nat.div n 10) with
+        | 0 => acc'
+        | n' => writeNatAux time' n' acc'
+      end
+  end.
+
+Definition writeNat (n : nat) : string :=
+  writeNatAux n n "".
+
+
+Notation "atoi( X )" := (readNat X) (at level 90).
+
+Notation "itoa( X )" := (writeNat X) (at level 90).
+
+Compute atoi("10").
+
+Compute itoa(10).
+
 (*Memory*)
 
 Definition nval (n : Value) : nat :=
@@ -169,6 +247,12 @@ Definition bval (b : Value) : bool :=
 match b with
 | bool_val b => b
 | _ => false
+end.
+
+Definition sval (s : Value) : string :=
+match s with
+| string_val s => s
+| _ => ""
 end.
 
 Definition State : Variabila := fun n => 0.
@@ -192,28 +276,72 @@ Definition is_declared (s : string) (state : Variabila) (values : Adresa) :=
   else true.
 
 Compute (updateState State "valAdresa" 0).
+Compute (updateState State "memPointer" 0)("memPointer").
 
 (*Exp*)
 
 Fixpoint expEval (e : Exp) (s : Variabila) (v : Adresa) : nat :=
-  match e with
-  | avar var => nval(v(s var))
-  | anum n' => n'
-  | btrue => 1
-  | bfalse => 0
-  | aplus e1 e2 => (expEval e1 s v) + (expEval e2 s v)
-  | aminus e1 e2 => (expEval e1 s v) - (expEval e2 s v)
-  | amul e1 e2 => (expEval e1 s v) * (expEval e2 s v) 
-  | adiv e1 e2 => Nat.div (expEval e1 s v) (expEval e2 s v)
-  | bless b1 b2 => if (Nat.ltb (expEval b1 s v) (expEval b2 s v)) then 1 else 0
-  | bgreat b1 b2=> if (Nat.ltb (expEval b2 s v) (expEval b1 s v)) then 1 else 0
-  | bleq b1 b2=> if (Nat.leb (expEval b1 s v) (expEval b2 s v)) then 1 else 0
-  | bgeq b1 b2=> if (Nat.leb (expEval b2 s v) (expEval b1 s v)) then 1 else 0
-  | beq b1 b2=> if (Nat.eqb (expEval b1 s v) (expEval b1 s v)) then 1 else 0
-  | bnot b' => match (expEval b' s v) with | 0 => 0 | _ => 1 end
-  | band b1 b2 => 0
-  | bor b1 b2=> if (Nat.eqb (expEval b1 s v) (expEval b2 s v)) then 1 else 0
-  end.
+match e with
+| avar var => nval(v(s var))
+| anum n' => n'
+| btrue => 1
+| bfalse => 0
+| aplus e1 e2 => (expEval e1 s v) + (expEval e2 s v)
+| aminus e1 e2 => (expEval e1 s v) - (expEval e2 s v)
+| amul e1 e2 => (expEval e1 s v) * (expEval e2 s v) 
+| adiv e1 e2 => Nat.div (expEval e1 s v) (expEval e2 s v)
+| bless b1 b2 => if (Nat.ltb (expEval b1 s v) (expEval b2 s v)) then 1 else 0
+| bgreat b1 b2=> if (Nat.ltb (expEval b2 s v) (expEval b1 s v)) then 1 else 0
+| bleq b1 b2=> if (Nat.leb (expEval b1 s v) (expEval b2 s v)) then 1 else 0
+| bgeq b1 b2=> if (Nat.leb (expEval b2 s v) (expEval b1 s v)) then 1 else 0
+| beq b1 b2=> if (Nat.eqb (expEval b1 s v) (expEval b1 s v)) then 1 else 0
+| bnot b' => match (expEval b' s v) with | 0 => 0 | _ => 1 end
+| band b1 b2 => 0
+| bor b1 b2=> if (Nat.eqb (expEval b1 s v) (expEval b2 s v)) then 1 else 0
+end.
 
+(*functions*)
 
+(*objects*)
 
+(*statements*)
+
+Fixpoint eval (pgm : Stmt) (s : Variabila) (v : Adresa) (gas : nat) : Adresa :=
+match gas with
+| 0 => v
+| S gas' => match s with
+            | var_declare var => if(is_declared var s v)
+                                 then v
+                                 else (updateValues v 
+                                      ((updateState
+                                      ((updateState s "memPointer" (s("memPointer")+1) var ("memPointer")))
+                                      "memPointer") undef)
+            | object_declare obj var =>
+            | function_declare funct stmt => functionEval funct stmt s v
+            | atoi_assignment var str => if(is_declared var s v)
+                                         then (updateValues v (s var) (nat_val atoi(str)))
+            | itoa_assignment var n => if(is_declared var s v)
+                                       then (updateValues v (s var) (string_val itoa(n))
+            | sequence stmt1 stmt2 => eval stmt2 (eval stmt1 env gas') gas'
+            | ifthen cond stmt =>  if (expEval cond s v)
+                                   then eval stmt s v gas'
+            | ifthenelse cond stmt stmt' => if (expEval cond s v)
+                                            then eval stmt s v gas'
+                                            else eval stmt s v gas'
+            | while cond s' => if (expEval cond s v)
+                               then eval (stmt' ;; (while cond stmt')) s v gas'
+                               else v
+            end 
+end.
+
+Inductive Stmt :=
+| var_declare : string -> Stmt 
+| object_declare : string -> Object -> Stmt
+| function_declare : Function -> Stmt -> Stmt
+| assignment : string -> Exp -> Stmt 
+| atoi_assignment : string -> string -> Stmt
+| itoa_assignment : string -> Exp -> Stmt
+| sequence : Stmt -> Stmt -> Stmt
+| ifthenelse : Exp -> Stmt -> Stmt -> Stmt
+| ifthen : Exp -> Stmt -> Stmt
+| while : Exp -> Stmt -> Stmt.
